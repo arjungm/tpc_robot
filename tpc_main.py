0d0df08733c18ad7dbae73010c029693329c2d7c
Atom Raiff <atom.raiff@gmail.com>
2017-01-30 08:23:43 -0800

    added communication with cozmo commander

--- tpc_main.py
+++ tpc_main.py
@@ -3,31 +3,41 @@
 import cozmo
 import requests
 
+def fetch_next_command():
+  print('fetching next command')
+  r = requests.get('http://cozmo-commander.herokuapp.com/queue').json()
+  return r['instruction']
+
 def cozmo_left(robot: cozmo.robot.Robot):
-	robot.turn_in_place(degrees(30)).wait_for_completed()
+  print('turning left')
+  robot.turn_in_place(degrees(30)).wait_for_completed()
 
 def cozmo_right(robot: cozmo.robot.Robot):
-	robot.turn_in_place(degrees(-30)).wait_for_completed()
+  print('turning right')
+  robot.turn_in_place(degrees(-30)).wait_for_completed()
 
 def cozmo_forward(robot: cozmo.robot.Robot):
-	robot.drive_straight( distance_mm(70), speed_mmps(50) ).wait_for_complete()
+  print('moving forward')
+  robot.drive_straight(distance_mm(70), speed_mmps(50)).wait_for_complete()
 
 def cozmo_backward(robot: cozmo.robot.Robot):
-	robot.drive_straight( distance_mm(-70), speed_mmps(50) ).wait_for_complete()
+  print('moving backward')
+  robot.drive_straight(distance_mm(-70), speed_mmps(50)).wait_for_complete()
 
 
-def main(robot: cozmo.robot.Robot)
-	command=1
-	while(true):
-		if(command==1):
-			cozmo_left()
-		elif(command==2):
-			cozmo_right()
-		elif(command==3):
-			cozmo_forward()
-		elif(command==4):
-			cozmo_backward()
-		command = command + 1
-		sleep(0.5)
+def main(robot: cozmo.robot.Robot):
+  command = fetch_next_command();
+  while(true):
+    if(command=='!cc-l'):
+      cozmo_left()
+    elif(command=='!cc-r'):
+      cozmo_right()
+    elif(command=='!cc-f'):
+      cozmo_forward()
+    elif(command=='!cc-b'):
+      cozmo_backward()
+    else:
+      print('cannot match the command "{}" to a comzo capability'.format(command))
+    sleep(0.5)
 
 cozmo.run_program(main)
